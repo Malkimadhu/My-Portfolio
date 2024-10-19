0babbd446982ee7ce7b554260180f00bb7ef5e82
malki <90499866+Malkimadhu@users.noreply.github.com>
2024-10-19 09:39:41 +0530

    New Updates 2024/10/19

--- src/components/Skills/Logo.jsx
+++ src/components/Skills/Logo.jsx
@@ -1,44 +1,12 @@
 import React from "react";
-import css from "../../assets/skills/css.png";
-import html from "../../assets/skills/html.png";
-import react from "../../assets/skills/react.png";
-import js from "../../assets/skills/js.png";
-import boostarp from "../../assets/skills/boostrap.png";
-import git from "../../assets/skills/git.png";
-import gitt from "../../assets/skills/Gitt.png";
-import figma from "../../assets/skills/figma.png";
-import postman from "../../assets/skills/postman.jpg";
-import canva from "../../assets/skills/canva.png";
 import "./skills.css";
 
-const Logo = () => {
+const Logo = ({name,logo,width,height}) => {
   return (
-    <section className="logo">
-        <div>
-      <img src={html} alt="HTML5 Logo" width="100" height="100"></img>
-      <img src={css} alt="CSS3 Logo" width="90" height="90"></img>
-      <img src={react} alt="React Logo" width="95" height="95"></img>
-      <img src={js} alt="JS Logo" width="100" height="100"></img>
-      <img src={boostarp} alt="Boostrap Logo" width="110" height="110"></img>
-      <img src={git} alt="Git hub Logo" width="120" height="120"></img>
-      <img src={gitt} alt="Git Logo" width="95" height="95"></img>
-      <img src={figma} alt="Figma Logo" width="90" height="90"></img>
-      <img src={postman} alt="Postman Logo" width="95" height="95"></img>
-      <img src={canva} alt="canva Logo" width="100" height="100"></img>
-      </div>
 
-      <div>
-      <img src={html} alt="HTML5 Logo" width="100" height="100"></img>
-      <img src={css} alt="CSS3 Logo" width="90" height="90"></img>
-      <img src={react} alt="React Logo" width="95" height="95"></img>
-      <img src={js} alt="JS Logo" width="100" height="100"></img>
-      <img src={boostarp} alt="Boostrap Logo" width="110" height="110"></img>
-      <img src={git} alt="Git hub Logo" width="120" height="120"></img>
-      <img src={html} alt="HTML5 Logo" width="100" height="100"></img>
-      <img src={css} alt="CSS3 Logo" width="90" height="90"></img>
-      <img src={react} alt="React Logo" width="95" height="95"></img>
-      <img src={js} alt="JS Logo" width="100" height="100"></img>
-      </div>
+    <section >
+      
+      <img className="logo__image" src={name} alt={logo} width={width} height={height}></img>
     </section>
     
   );
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,6 +3,24 @@ import "./skills.css";
 import FrontEnd from "./FrontEnd";
 import BackEnd from "./Backend";
 import Logo from "./Logo";
+import css from "../../assets/skills/css.png";
+import html from "../../assets/skills/html.png";
+import react from "../../assets/skills/react.png";
+import js from "../../assets/skills/js.png";
+import boostarp from "../../assets/skills/boostrap.png";
+import git from "../../assets/skills/git.png";
+import gitt from "../../assets/skills/Gitt.png";
+import figma from "../../assets/skills/figma.png";
+import postman from "../../assets/skills/postman.jpg";
+import canva from "../../assets/skills/canva.png";
+import php from "../../assets/skills/php.png";
+import java from "../../assets/skills/java.png";
+import spring from "../../assets/skills/spring.png";
+import sql from "../../assets/skills/sql.png";
+import python from "../../assets/skills/python.png";
+import dj from "../../assets/skills/dj.png";
+import vs from "../../assets/skills/vs.png";
+import ij from "../../assets/skills/ij.png";
 
 const Skills = () => {
   return (
@@ -10,7 +28,26 @@ const Skills = () => {
       <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">My Technical Level</span>
 
-      <Logo />
+<section className="logo">
+      <Logo name={html} logo="HTML5 Logo" width={100} height={100}/>
+      <Logo name={css} logo="css Logo" width={100} height={100}/>
+      <Logo name={react} logo="React Logo" width={100} height={100}/>
+      <Logo name={js} logo="Java Script Logo" width={100} height={100}/>
+      <Logo name={boostarp} logo="Boostrap Logo" width={100} height={100}/>
+      <Logo name={git} logo="Git hub Logo" width={100} height={100}/>
+      <Logo name={gitt} logo="Git Logo" width={100} height={100}/>
+      <Logo name={figma} logo="Figma Logo" width={100} height={100}/>
+      <Logo name={postman} logo="Postman Logo" width={100} height={100}/>
+      <Logo name={canva} logo="Cnva Logo" width={100} height={100}/>
+      <Logo name={php} logo="Php Logo" width={100} height={100}/>
+      <Logo name={java} logo="Java Logo" width={100} height={100}/>
+      <Logo name={spring} logo="Spring Logo" width={100} height={100}/>
+      <Logo name={sql} logo="MySQL Logo" width={100} height={100}/>
+      <Logo name={python} logo="Python Logo" width={100} height={100}/>
+      <Logo name={dj} logo="Django Logo" width={100} height={100}/>
+      <Logo name={vs} logo="VSCode Logo" width={100} height={100}/>
+      <Logo name={ij} logo="Intelij IDEA Logo" width={100} height={100}/>
+      </section>
       <div className="skills__container container grid">
         <FrontEnd />
         <BackEnd />
